cd424a6b14a85302391bc0c32c76a2dc0a1de036
Hong-ryul Jung <jung.hr.1206@gmail.com>
2020-06-08 05:31:06 +0900

    Fixed: oh-my-zsh redirection error

--- components/oh-my-zsh.sh
+++ components/oh-my-zsh.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 source <(curl -fsSL https://raw.githubusercontent.com/ryul1206/setting-my-env/master/functions.sh)
 
-
 (subsection "oh-my-zsh")
 
 cd
 if [ -d ".oh-my-zsh" ]; then
     already-installed "oh-my-zsh"
 else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # Redirection Problem Solved!
+    # https://stackoverflow.com/questions/37360258/unreachable-command-in-a-shell-script-code-while-installing-oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed "s/exec zsh.*//g")"
 
     ####################
     # oh-my-zsh plugins
@@ -17,7 +18,6 @@ else
     (subsection "zsh-autosuggestions")
     # https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    
 
     sed -i 's/plugins=(git)/plugins=(\n  git\n  zsh-autosuggestions\n)/g' ~/.zshrc
 fi
