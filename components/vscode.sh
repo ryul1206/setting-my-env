ae807453990e29e4d9569215b8f5c27727287b66
Hong-ryul Jung <jung.hr.1206@gmail.com>
2020-08-04 18:11:53 +0900

    Update vscode installation with snap

--- components/vscode.sh
+++ components/vscode.sh
@@ -7,14 +7,5 @@ source <(curl -fsSL https://raw.githubusercontent.com/ryul1206/setting-my-env/ma
 if [ "$(which code)" ]; then
     already-installed "vscode"
 else
-    cd
-    GPG_FILE="packages.microsoft.gpg"
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >$GPG_FILE
-    sudo install -o root -g root -m 644 $GPG_FILE /usr/share/keyrings/
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-    sudo apt-get install apt-transport-https
-    sudo apt-get update
-    sudo apt-get install code
-    rm $GPG_FILE
+    sudo snap install code --classic
 fi
