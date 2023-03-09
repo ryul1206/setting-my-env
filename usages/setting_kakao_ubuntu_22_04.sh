161c8a4fc4e587cb8b29060286e61b6701e45fcb
Jung, Hong-ryul <jung.hr.1206@gmail.com>
2023-03-09 14:36:41 +0900

    Fix typos

--- usages/setting_kakao_ubuntu_22_04.sh
+++ usages/setting_kakao_ubuntu_22_04.sh
@@ -3,13 +3,16 @@ source <(curl -fsSL ${REPOSITORY_URL}/functions.sh)
 
 (section-separator "Switch the source to the Kakao server and update")
 
-SOURCE_LIST=/etc/apt/source.list  
+SOURCE_LIST=/etc/apt/sources.list
 BAK_PREFIX=.bak.original
 
 # Switch the source, only when a backup does not exist
 if [ ! -f "$SOURCE_LIST$BAK_PREFIX" ]
 then
-	sed -i$BAK_PREFIX -re "s/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/mirror.kakao.com/g" $SOURCE_LIST
+	sudo sed -i$BAK_PREFIX -re "s/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/mirror.kakao.com/g" $SOURCE_LIST
+	(emphasis "DONE! archive.ubuntu.com|security.ubuntu.com -> mirror.kakao.com")
+else
+	(emphasis "SKIPPED. (Already your setup is mirror.kakao.com)")
 fi
 
 (emphasis "sudo apt update")
